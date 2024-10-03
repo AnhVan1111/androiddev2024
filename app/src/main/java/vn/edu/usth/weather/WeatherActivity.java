1b23221bc09588e01c5cfc9dfd6e8cde868cc553
AnhVan1111 <anhtv.ba12-007@st.usth.edu.vn>
2024-10-04 06:11:00 +0700

    Practice 12

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,16 +3,21 @@ package vn.edu.usth.weather;
 import android.os.Bundle;
 import android.util.Log;
 
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.widget.Toolbar;
 
-import android.media.MediaPlayer;
-import android.os.Environment;
-import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -34,15 +39,41 @@ public class WeatherActivity extends AppCompatActivity {
 
         HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Log.i(Tag, "onCreate");
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_weather, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.refresh_button) {
+            Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.setting_button) {
+            Intent intent = new Intent(this, PrefActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void extractAndPlayMusic() {
         File musicFile = new File(Environment.getExternalStorageDirectory(), "sample.mp3");
         if (!musicFile.exists()) {
